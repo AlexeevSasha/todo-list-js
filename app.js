07a7d938f9503f9b7e6a75d9582ac48fea6f054c
AlexeevSasha <work.alexeevs@gmail.com>
2021-10-01 13:49:12 +0300

    add completed task

--- app.js
+++ app.js
@@ -44,11 +44,16 @@ const tasks = [
  const form = document.forms['addTask'];
  const inputTitle = form.elements['title'];
  const inputBody = form.elements['body'];
+ const messageNoTaskHtml = document.querySelector('.task');
+ let showSortTask = false;
+
 
 // Events
   renderAlltasks(objOfTasks);
   form.addEventListener('submit', onFormSubmitHandler)
   listContainer.addEventListener('click', onDeleteHandler)
+  listContainer.addEventListener('click', onCompletedHandler)
+
 
 
 // Добавление tasks в listContainer
@@ -68,18 +73,27 @@ const tasks = [
   }
 
 // Создание разметки
-  function listItemTempleta( { _id, title , body} = {}) {
+  function listItemTempleta( { _id, title , body, completed} = {}) {
     const li = document.createElement('li');
     li.classList.add('list-group-item', 'd-flex', 'align-items-center', 'flex-wrap', 'mt-2');
     li.setAttribute('data-task-id', _id)
 
+
     const span = document.createElement('span');
+    span.classList.add('title');
     span.textContent = title;
     span.style.fontWeight = 'bold';
 
     const deleteBtn = document.createElement('button');
     deleteBtn.classList.add('btn', 'btn-danger', 'ml-auto', 'delete-btn')
     deleteBtn.textContent = 'Delete task'
+
+    const completedBtn = document.createElement('button')
+    completedBtn.classList.add('completed-btn')
+    completedBtn.textContent = 'Completed'
+    if (completed) {
+      li.classList.add('color')
+    }
 
     const article = document.createElement('p');
     article.classList.add('mt-2', 'w-100')
@@ -88,6 +102,8 @@ const tasks = [
     li.appendChild(span);
     li.appendChild(deleteBtn);
     li.appendChild(article);
+    li.appendChild(completedBtn);
+
 
     return li;
   }
@@ -105,6 +121,7 @@ const tasks = [
     const task = createNewTask(titleValue, bodyValue)
     const listItem = listItemTempleta(task);
     listContainer.insertAdjacentElement('afterbegin', listItem)
+    messageNoTask()
     form.reset();
   }
 
@@ -122,13 +139,15 @@ const tasks = [
     return { ...newTask };
   }
 
-//Объект событыия для listContainer
+//Объект событыия (удаление task) для listContainer
   function onDeleteHandler({target}) {
     if (target.classList.contains('delete-btn')) {
       const parent = target.closest('[data-task-id]')
       const id = parent.dataset.taskId;
       const confirmed =  returnsConfirm(id);
       deleteTaskFormHtml(confirmed, parent)
+      messageNoTask()
+
     }
   }
 
@@ -147,14 +166,83 @@ const tasks = [
     return isConfirm;
   }
 
+//Если нет ни одно задачи появляеться плашка
+ function messageNoTask() {
+   if (Array.from(listContainer.children).length != 0) {
+    messageNoTaskHtml.classList.remove('task-block')
+   } else {
+    messageNoTaskHtml.classList.add('task-block')
+   }
+ }
+
+//Объект событыия (завершение task) для listContainer
+function onCompletedHandler( {target}) {
+  if (target.classList.contains('completed-btn')) {
+    const parent = target.closest('[data-task-id]')
+    const id = parent.dataset.taskId;
+    parent.classList.toggle('color')
+
+    if(objOfTasks[id].completed === true) {
+      objOfTasks[id].completed = false
+    } else {
+      objOfTasks[id].completed = true;
+    }
+  }
+  sortcompletedTask();
+}
 
 
+//All tasks фильтр
+const allBtn = document.querySelector('.all-btn');
+allBtn.addEventListener('click', () => {
+  let arr = Array.from(listContainer.children)
+  arr.forEach(item => {
+    item.classList.remove('displayNone');
+  })
+  allBtn.classList.add('all-btn--active')
+  noCompletedBtn.classList.remove('nocompleted-btn--active')
+  document.querySelector('.nocompleted-task').classList.remove('nocompleted-block')
+  showSortTask  = false
+})
+
+//No Completed tasks фильтр
+const noCompletedBtn = document.querySelector('.nocompleted-btn');
+noCompletedBtn.addEventListener('click', () => {
+  allBtn.classList.remove('all-btn--active')
+  noCompletedBtn.classList.add('nocompleted-btn--active')
+  completedTask();
+  let arr = Array.from(listContainer.children);
+  let arrFilter = arr.filter(item => !item.classList.contains('displayNone'))
+  if (!arrFilter.length) {
+      document.querySelector('.nocompleted-task').classList.add('nocompleted-block')
+  }
+
+})
+
+function completedTask () {
+  let arr = Array.from(listContainer.children)
+  let sortList = arr.filter(item => {
+    return objOfTasks[item.dataset.taskId].completed === true
+  });
+  sortList.forEach(item => {
+    item.classList.add('displayNone')
+  })
+  showSortTask = true;
+}
+
+//Сортирует элементы на завершёные и незавершнные
+
+function sortcompletedTask() {
+  let arr = Array.from(listContainer.children);
+  let sortList = arr.sort( (a,b) => {
+    return objOfTasks[a.dataset.taskId].completed > objOfTasks[b.dataset.taskId].completed ? 1 : -1;
+  })
+  arr.forEach(item => item.remove())
+  sortList.forEach(item => listContainer.appendChild(item))
+}
+sortcompletedTask();
 
 })(tasks);
-
-
-
-
 
 
 
